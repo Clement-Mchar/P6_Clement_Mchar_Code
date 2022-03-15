b431b7cd3c5c4500d70e889016a2f706d4ca010e
Clement M'char <clement.mchar@gmail.com>
2022-03-15 18:30:20 +0100

    Réglage bug nom des sauces

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -3,21 +3,23 @@ const Sauce = require("../models/sauce");
 const fs = require("fs");
 
 exports.createSauce = (req, res, next) => {
-	const sauceObject = JSON.parse(req.body.sauce)
+	const sauceObject = JSON.parse(req.body.sauce);
 	delete sauceObject._id;
 	const sauce = new Sauce({
-	  ...sauceObject,
-	  imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-	  likes: 0,
-	  dislikes: 0,
-	  usersLiked: [' '],
-	  usersdisLiked: [' '],
+		...sauceObject,
+		imageUrl: `${req.protocol}://${req.get("host")}/images/${
+			req.file.filename
+		}`,
+		likes: 0,
+		dislikes: 0,
+		usersLiked: [" "],
+		usersdisLiked: [" "],
 	});
 	sauce
-	  .save()
-	  .then(() => res.status(201).json({ message: "Sauce enregistrée" }))
-	  .catch((error) => res.status(400).json({ error }));
-  };
+		.save()
+		.then(() => res.status(201).json({ message: "Sauce enregistrée" }))
+		.catch((error) => res.status(400).json({ error }));
+};
 
 exports.getOneSauce = (req, res, next) => {
 	Sauce.findOne({
@@ -34,6 +36,12 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
+	Sauce.findOne({ _id: req.params.id })
+		.then((sauce) => {
+			const filename = sauce.imageUrl.split("/images/")[1];
+			fs.unlink(`images/${filename}`, () => {});
+		})
+		.catch((error) => res.status(500).json({ error }));
 	const sauceObject = req.file
 		? {
 				...JSON.parse(req.body.sauce),
